8e249ffd9e5da6b39e49b78767a63e6d280ad027
yogeshdeved <106949860+yogeshdeved@users.noreply.github.com>
2022-06-10 11:32:43 +0530

    Create products.js

---
+++ routes/products.js
@@ -0,0 +1,26 @@
+const product = require("../models/products");
+const router=require("express").Router();
+
+router.get("/",async (req,res)=>{
+    try
+    {
+    const dbr = await product.find();
+    res.status(200).json(dbr);
+    }
+    catch(error)
+    {
+        res.status(500,json({message:error}))
+    }
+})
+router.post("/add", async (req, res) => {
+    const newProduct = new category(req.body);
+  
+    try {
+      const savedProduct = await newProduct.save();
+      res.status(200).json(savedProduct);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+module.exports =router
