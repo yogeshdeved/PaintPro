b54b5cf07a90fd8d6eedad44c032a5e3efa51e11
yogeshdeved <106949860+yogeshdeved@users.noreply.github.com>
2022-06-17 12:21:28 +0530

    Update kit.js

--- models/kit.js
+++ models/kit.js
@@ -1,8 +1,8 @@
 const mongoose=require("mongoose");
-const Component=new mongoose.Schema({
+const Kit=new mongoose.Schema({
     name : {type:String, required:true, unique:true},
     description : {type:String, required:false, unique:false},
-    componenttype: {type:String, required:false,unique:false},
+    kittype: {type:String, required:false,unique:false},
     parent : {type:String, required:true, unique:false, default:''},
     status : {type:Boolean, required:true, default:false},
     rank_order : {type:Number, required:false},
@@ -12,4 +12,4 @@ const Component=new mongoose.Schema({
     created_on : {type:String, required:true, default:Date.now},
     is_deleted : {type:Boolean, required:true, default:false}
 })
-module.exports=mongoose.model("Component",Component);
+module.exports=mongoose.model("Kit",Kit);
