7542fd7cf0f95a78afc52d3ebe3ac300aff00244
yogeshdeved <106949860+yogeshdeved@users.noreply.github.com>
2022-06-17 11:33:58 +0530

    Update productCategory.js

--- models/productCategory.js
+++ models/productCategory.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose");
 const productCategory=new mongoose.Schema({
     name : {type:String, required:true, unique:true},
     description : {type:String, required:false, unique:false},
-    ctype: {type:string, required=true,unique:false},
+    ctype: {type:string, required:true,unique:false},
     parent : {type:string, required:true, unique:false, default:''},
     status : {type:Boolean, required:true, default:false},
     rank_order : {type:Number, required:true},
