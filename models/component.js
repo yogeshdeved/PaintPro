79b805294856a218a525aa1987c68121083df8f8
yogeshdeved <106949860+yogeshdeved@users.noreply.github.com>
2022-06-17 12:17:32 +0530

    Create component.js

---
+++ models/component.js
@@ -0,0 +1,19 @@
+const mongoose=require("mongoose");
+const productCategory=new mongoose.Schema({
+    name : {type:String, required:true, unique:true},
+    description : {type:String, required:false, unique:false},
+    ctype: {type:String, required:true,unique:false},
+    parent : {type:String, required:true, unique:false, default:''},
+    status : {type:Boolean, required:true, default:false},
+    rank_order : {type:Number, required:true},
+    url : {type:String, required:true, unique:true},
+    
+    updated_on : {type:String, required:true, default:Date.now},
+    created_on : {type:String, required:true, default:Date.now},
+    is_deleted : {type:Boolean, required:true, default:false},
+    banner:{
+        title:{type:String, required:true},
+        image:{type:String, required:true}
+    }
+})
+module.exports=mongoose.model("ProductCategory",productCategory);
