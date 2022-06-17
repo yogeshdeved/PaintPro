a25458a94ba967de474818c78d12802ceb6f34f4
yogeshdeved <106949860+yogeshdeved@users.noreply.github.com>
2022-06-17 16:28:21 +0530

    Update category.js

--- routes/category.js
+++ routes/category.js
@@ -4,7 +4,18 @@ const router=require("express").Router();
 router.get("/",async (req,res)=>{
     try
     {
-    const dbr = await category.find();
+    const dbr = await category.find({"ctype":"Category"});
+    res.status(200).json(dbr);
+    }
+    catch(error)
+    {
+        res.status(500,json({message:error}))
+    }
+})
+router.get("/:pid",async (req,res)=>{
+    try
+    {
+    const dbr = await category.find({"ctype":req.params.pid});
     res.status(200).json(dbr);
     }
     catch(error)
