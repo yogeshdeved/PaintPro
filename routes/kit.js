aac5dbb4513057ca4e4487698b68d994297477e1
yogeshdeved <106949860+yogeshdeved@users.noreply.github.com>
2022-06-17 13:02:44 +0530

    Create kit.js

---
+++ routes/kit.js
@@ -0,0 +1,26 @@
+const kit = require("../models/Kit");
+const router=require("express").Router();
+
+router.get("/",async (req,res)=>{
+    try
+    {
+    const dbr = await kit.find();
+    res.status(200).json(dbr);
+    }
+    catch(error)
+    {
+        res.status(500,json({message:error}))
+    }
+})
+router.post("/add", async (req, res) => {
+    const newProduct = new kit(req.body);
+  
+    try {
+      const savedProduct = await newProduct.save();
+      res.status(200).json(savedProduct);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+module.exports =router
